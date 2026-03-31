29cdc0ea5b54eef93f730569117dadf1f05039e1
Siddharthxvi <192679817+Siddharthxvi@users.noreply.github.com.>
2026-03-31 12:19:11 +0530

    Deploying backend for Render

--- core/config.py
+++ core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://root:password@localhost:5432/portfolio_risk_db"
+    DATABASE_URL: str = "postgresql://risk_platform_gcga_user:olxEJdFWKnzf1gG7zoCiqZ0qKrEtrk6V@dpg-d75mgrgule4c73ctlj0g-a.singapore-postgres.render.com/risk_platform_gcga?sslmode=require"
 
     class Config:
         env_file = ".env"
